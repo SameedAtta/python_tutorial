b126bd82117820484521ab1e4dcb35f1b90407f8
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-26 18:30:15 +0500

    Commenting convention 3

---
+++ python_comments/exercise_3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 18:28:48 2022
+
+@author: sameed.atta
+"""
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
