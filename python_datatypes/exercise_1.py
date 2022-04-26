5b163d730babb42f1198a7d126aa22bcee5cf628
SameedAtta <95316471+SameedAtta@users.noreply.github.com>
2022-04-26 21:30:51 +0500

    Python datatypes (#5)
    * DataType 1
    
    * DataType 2
    
    * Type Casting convention
    
    * converting convention

---
+++ python_datatypes/exercise_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 20:53:47 2022
+
+@author: sameed.atta
+"""
+
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ python_datatypes/exercise_4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 21:18:35 2022
+
+@author: sameed.atta
+"""
+
+print(f"{3 ** .125:.3f}")
+print(f"${150000:,.2f}")
+print(f"{2 / 10:.0%}")
