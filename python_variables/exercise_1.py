c87f69b0f301314375124b1a04cfa3e2a947c793
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-26 19:48:15 +0500

    variable naming convention 1

---
+++ python_variables/exercise_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 19:41:43 2022
+
+@author: sameed.atta
+"""
+
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string
+print (counter)
+print (miles)
+print (name)
