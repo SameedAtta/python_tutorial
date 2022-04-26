57dddf10244aa2ce3b7f3128e9598be648ae88ea
SameedAtta <95316471+SameedAtta@users.noreply.github.com>
2022-04-26 20:47:37 +0500

    Python variables (#4)
    * variable naming convention 1
    
    * variable naming convention 2
    
    * variable naming convention 3
    
    * variable naming convention 4
    
    * variable naming convention 5

---
+++ python_variables/exercise_3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 19:58:47 2022
+
+@author: sameed.atta
+"""
+
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
