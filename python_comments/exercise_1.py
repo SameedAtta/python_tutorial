fac694ad03fa94953ff164eabfefd49a2e361785
SameedAtta <95316471+SameedAtta@users.noreply.github.com>
2022-04-26 18:34:36 +0500

    Python comments (#2)
    * Commenting convention 1
    
    * Commenting convention 2
    
    * Commenting convention 3

---
+++ python_comments/exercise_1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 18:15:37 2022
+
+@author: sameed.atta
+"""
+
+# print("hello world")
---
+++ python_comments/exercise_2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 18:26:19 2022
+
+@author: sameed.atta
+"""
+
+#print("Hello, World!")
+print("Cheers, Mate!")
