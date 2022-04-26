91ef8e0acce644095a07ab08a4561517bd070e33
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-26 21:49:53 +0500

    String indexing and Slicin

---
+++ python_strings/exercise_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 21:48:47 2022
+
+@author: sameed.atta
+"""
+
+# Concatenate two strings together
+string_left = "bat"
+string_right = "man"
+print(string_left + string_right)
