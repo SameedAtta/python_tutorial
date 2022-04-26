7b742ffb3e33d99eeb9c5923b3c13191c4ec3ae6
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-26 20:09:46 +0500

    variable naming convention 5

---
+++ python_variables/exercise_5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 20:08:34 2022
+
+@author: sameed.atta
+"""
+
+# Get two numbers from the user, multiply them,
+# and display the result
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+product = float(a) * float(b)
+print("The product of " + a + " and " + b + " is " + str(product) + ".")
