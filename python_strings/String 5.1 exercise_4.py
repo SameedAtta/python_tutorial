5696bdf910e5d3a8f49152043666a57f59f8c3e9
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 14:47:54 +0500

    .Startwith() function

---
+++ python_strings/String 5.1 exercise_4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 14:46:23 2022
+
+@author: sameed.atta
+"""
+
+# Exercise 4
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = " bEautiful"
+print(string1.startswith("be"))
+print(string2.startswith("be"))
+print(string3.startswith("be"))
+print(string4.startswith("be"))
