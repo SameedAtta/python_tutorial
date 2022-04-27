f6d6c4234904ecd18bfc68a76a798c7c47356391
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 18:30:25 +0500

    assignment of function parameter

---
+++ python_functions/exercise_2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 18:29:38 2022
+
+@author: sameed.atta
+"""
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
