8d9d79290cf7721435afdd37281022f3f59e9bcc
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 17:13:21 +0500

    while loop detailed

---
+++ python_loops/loops_exercise_3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 17:11:49 2022
+
+@author: sameed.atta
+"""
+
+stuff = ['hello', 'hi', 'how are you doing', 'im fine', 'how about you']
+length_of_stuff = len(stuff)
+index = 0
+while index < length_of_stuff:
+    print(stuff[index])
+    index += 1
