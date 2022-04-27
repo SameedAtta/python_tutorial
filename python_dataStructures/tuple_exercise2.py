d77183bc6e25a608ce519f54e2c379c947037e1c
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 16:10:12 +0500

    - indices range

---
+++ python_dataStructures/tuple_exercise2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 16:07:44 2022
+
+@author: sameed.atta
+"""
+
+# This example returns the items from the beginning to, but NOT included, "kiwi"
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+# This example returns the items from index -4 (included) to index -1 (excluded)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
