f2d2278fe7b132a0b068dd6c89ce04ea3a85ffb7
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 14:40:47 +0500

    Upper case convention

---
+++ python_strings/String 5.1 exercise_2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 14:38:19 2022
+
+@author: sameed.atta
+"""
+
+
+
+
+
+string1 = "Animals"
+string2 = "Badger"
+string3 = "Honey Bee"
+string4 = "Honeybadger"
+print(string1.upper())
+print(string2.upper())
+print(string3.upper())
+print(string4.upper())
