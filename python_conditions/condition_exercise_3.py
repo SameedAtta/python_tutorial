fb2ff83e3c5b9f132a504fc8c4fed763ae1e0ce8
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 16:52:45 +0500

    if and else amd elseif statements

---
+++ python_conditions/condition_exercise_3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 16:41:45 2022
+
+@author: sameed.atta
+"""
+
+# Nested If
+# You can have if statements inside if statements, this is called nested if statements.
+x = 41
+if x > 10:
+print("Above ten,")
+if x > 20:
+print("and also above 20!")
+else:
+print("but not above 20.")
+# if statements cannot be empty, but if you for some reason have an if statement
+# with no content, put in the pass statement to avoid getting an error
+a = 33
+b = 200
+if b > a:
+pass
