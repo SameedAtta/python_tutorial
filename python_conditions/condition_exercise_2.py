c74dd01217139655d70bae56aa419a2c18c5fad3
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 16:51:40 +0500

    if and else statements

---
+++ python_conditions/condition_exercise_2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 16:41:22 2022
+
+@author: sameed.atta
+"""
+
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+# One line if else statement, with 3 conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
