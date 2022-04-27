289d4218803e8feb9537c56d82960df26240b232
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 16:12:17 +0500

    join tuples

---
+++ python_dataStructures/tuple_exercise3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 16:11:33 2022
+
+@author: sameed.atta
+"""
+
+# Join tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
