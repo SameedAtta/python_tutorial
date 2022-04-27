4fb03be8848817ae38840513f88c1333148e73b1
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 16:07:07 +0500

    properties of tuple

---
+++ python_dataStructures/tuple_exercise1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 16:05:16 2022
+
+@author: sameed.atta
+"""
+
+# Create a Tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# Print the number of items in the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+# One item tuple, remember the commma:
+thistuple = ("apple",)
+print(type(thistuple))
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+tuple1 = ("abc", 34, True, 40, "male")
+print(tuple1)
