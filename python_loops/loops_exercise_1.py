311d0aa8f093b0aac665eb56ea8c285f12eded7d
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 17:09:44 +0500

    while loop

--- python_loops/loops_exercise_1.py
+++ python_loops/loops_exercise_1.py
@@ -5,3 +5,9 @@ Created on Wed Apr 27 16:59:30 2022
 @author: sameed.atta
 """
 
+# Print all items, using a while loop to go through all the index numbers
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
