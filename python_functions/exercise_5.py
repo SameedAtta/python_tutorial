3f11b6f92d281250b38f0475fc6a442bb4808154
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 20:11:30 +0500

    gravititional force function

--- python_functions/exercise_5.py
+++ python_functions/exercise_5.py
@@ -4,4 +4,10 @@ Created on Wed Apr 27 19:48:37 2022
 
 @author: sameed.atta
 """
-
+'''make gravitational function'''
+mass1 = float(input("First mass: "))
+mass2 = float(input("Second mass: "))
+r = float(input("Distance between the objects: "))
+G = 6.673*(10**-11)
+force =(G*mass1*mass2)/(r**2)
+print("The gravitational force is:", round(force, 5),"N")
