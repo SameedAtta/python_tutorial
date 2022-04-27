c1761eddff671c0a4539fa6ea1a1498105fbba57
sameed.atta.khan <sameed.atta.khan99@gmail.com>
2022-04-27 14:44:30 +0500

    .Strip() function

---
+++ python_strings/String 5.1 exercise_3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 14:41:35 2022
+
+@author: sameed.atta
+"""
+
+# Exercise 3
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+print(string1.strip()) # Could also use .lstrip()
+print(string1.strip()) # Could also use .rstrip()
+print(string3.strip())
